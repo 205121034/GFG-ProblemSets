bde062b7b476cac88f1aeedf43a729172afb6564
BIKASH KUMAR YADAV <93582708+205121034@users.noreply.github.com>
2025-12-29 22:01:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,56 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int kthElement(vector<int>& a, vector<int>& b, int k) {
-        vector<int>ans;
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        // code here
+        vector<int> mergerArr ; 
         
-        for(int i=0;i<a.size();i++)
-           ans.push_back(a[i]);
-           
-        for(int i=0;i<b.size();i++)
-          ans.push_back(b[i]);
-         
-       sort(ans.begin(),ans.end());
-       
-       return ans[k-1];
+        for(int i = 0 ; i < a.size() ; i++) 
+        {
+            mergerArr.push_back(a[i]);
+        }
+        
+        
+        for(int i = 0 ; i < b.size(); i++){
+            mergerArr.push_back(b[i]);
+        }
+        
+        sort(mergerArr.begin() , mergerArr.end());
+        
+        return mergerArr[k-1]; // Because of Zero based Indexing
+        
+        //TC- O(n+m log(n + m));
+        //SC- O(m + n); 
+        
     }
 };
-
-//{ Driver Code Starts.
-
-// Driver code
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, m, k;
-        cin >> k;
-        cin.ignore();
-        string input;
-        int num;
-        vector<int> a, b;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-
-        getline(cin, input);
-        stringstream s3(input);
-        while (s3 >> num) {
-            b.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.kthElement(a, b, k) << endl << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
